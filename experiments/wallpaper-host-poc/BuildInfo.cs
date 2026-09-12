11648b9cac265d87b2e2ef53452eb366851c1bd6
yuna <114085532+jeyun123456@users.noreply.github.com>
2026-09-13 07:39:40 +0900

    chore(wallpaper): finalize v0.5 product identity

--- experiments/wallpaper-host-poc/BuildInfo.cs
+++ experiments/wallpaper-host-poc/BuildInfo.cs
@@ -2,7 +2,9 @@ namespace WallpaperHostPoc;
 
 internal static class BuildInfo
 {
-    internal const string ProductName = "Master Thesis OS Wallpaper";
+    internal const string ProductName = "Master Thesis OS Wallpaper Companion";
+    internal const string ShortProductName = "Master Thesis OS Wallpaper";
+    internal const string ExecutableName = "MasterThesisOSWallpaper.exe";
 
     internal static string Version =>
         typeof(BuildInfo).Assembly.GetName().Version?.ToString(3) ?? "0.0.0";
